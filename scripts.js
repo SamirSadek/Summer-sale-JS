0862f5af43981dd43c106e476747afb9cc9e08f6
Samir Sadek <samirsadek2009@gmail.com>
2023-08-21 21:54:36 +0600

    Assignment Done

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
-
 let total = 0;
-let formattedPrice = 0;
-
-
-
 function addToEntry(target) {
 
     const selectedItems = document.getElementById('selected-items');
@@ -16,39 +11,62 @@ function addToEntry(target) {
     const priceString = target.childNodes[3].childNodes[5].childNodes[0].innerText;
     const priceFloat = parseFloat(priceString);
 
-    // Format priceFloat to have two decimal places
     const formattedPrice = priceFloat.toFixed(2);
-    // Add formattedPrice to the total
     total = parseFloat(total) + parseFloat(formattedPrice);
 
     const formattedTotal = total.toFixed(2);
     document.getElementById('totalPrice').innerText = formattedTotal;
     document.getElementById('total').innerText = formattedTotal;
-    console.log(formattedPrice);
-    console.log(formattedTotal);
 
 }
 
-    function couponCode(target) {
-        const inputCoupon = target.parentNode.childNodes[1].value;
-        const totalPrice = document.getElementById('totalPrice');
-        const totalPriceValue = totalPrice.innerText;
-        const totalPriceFloat = parseFloat(totalPriceValue);
-        console.log(totalPriceFloat, inputCoupon);
-        if (inputCoupon == "SELL200" && totalPriceFloat >= 200) {
-            const discountPrice = totalPriceFloat * (1 / 5);
-            const priceAfterDiscount = totalPriceFloat - discountPrice;
-            const formattedDiscountPrice = discountPrice.toFixed(2);
-            const formattedPriceAfterDiscount = priceAfterDiscount.toFixed(2);
 
-            document.getElementById('discount').innerText = formattedDiscountPrice;
-            document.getElementById('total').innerText = formattedPriceAfterDiscount;
+function couponCode(target) {
+    const inputCoupon = target.parentNode.childNodes[1].value;
+    const totalPrice = document.getElementById('totalPrice');
+    const totalPriceValue = totalPrice.innerText;
+    const totalPriceFloat = parseFloat(totalPriceValue);
+    console.log(totalPriceFloat, inputCoupon);
 
-            console.log(formattedPriceAfterDiscount);
+    if (inputCoupon == "SELL200" && totalPriceFloat >= 200) {
+        const discountPrice = totalPriceFloat * (1 / 5);
+        const priceAfterDiscount = totalPriceFloat - discountPrice;
+        const formattedDiscountPrice = discountPrice.toFixed(2);
+        const formattedPriceAfterDiscount = priceAfterDiscount.toFixed(2);
+        document.getElementById('discount').innerText = formattedDiscountPrice;
+        document.getElementById('total').innerText = formattedPriceAfterDiscount;
+        console.log(formattedPriceAfterDiscount);
 
-        } else {
-            alert("Requirements Doesn't match");
-        }
+    } else {
+        alert("Requirements Doesn't match");
     }
+}
 
+
+const totalPriceSpan = document.getElementById("totalPrice");
+const purchaseBtn = document.getElementById("purchaseBtn");
+let button = document.getElementById('applyBtn');
+button.disabled = true;
+totalPriceSpan.addEventListener("DOMSubtreeModified", function() {
+    const totalPrice = parseFloat(totalPriceSpan.innerText);
+    if (totalPrice >= 200) {
+        button.removeAttribute("disabled");
+        button.classList.remove("cursor-not-allowed", "opacity-50");
+    } else {
+        button.setAttribute("disabled", true);
+        button.classList.add("cursor-not-allowed", "opacity-50");
+    }
+});
+    
+
+totalPriceSpan.addEventListener("DOMSubtreeModified", function() {
+    const totalPrice = parseFloat(totalPriceSpan.innerText);
+    if (totalPrice > 0) {
+        purchaseBtn.removeAttribute("disabled");
+        purchaseBtn.classList.remove("cursor-not-allowed", "opacity-50");
+    } else {
+        purchaseBtn.setAttribute("disabled", true);
+        purchaseBtn.classList.add("cursor-not-allowed", "opacity-50");
+    }
+});
 
